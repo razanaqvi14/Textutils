d40dbc4e8bf1b7027bd717531e5dac02b49e91ef
Syed Mohammad Ali Raza Naqvi <razanaqvi2004@gmail.com>
2023-06-03 15:44:33 +0500

    Did some changes in textutils

--- src/Components/DismissingAlert.js
+++ src/Components/DismissingAlert.js
@@ -5,16 +5,18 @@ export const DismissingAlert = (props) => {
     return word.charAt(0).toUpperCase() + word.slice(1);
   };
   return (
-    props.alert && (
-      <div className="container my-4">
-        <div
-          className={`alert alert-${props.alert.alertType} alert-dismissible fade show`}
-          role="alert"
-        >
-          <strong>{capitalize(props.alert.alertType)}: </strong>
-          {props.alert.alertMessage}
+    <div style={{ height: "70px" }}>
+      {props.alert && (
+        <div className="container">
+          <div
+            className={`alert alert-${props.alert.alertType} alert-dismissible fade show`}
+            role="alert"
+          >
+            <strong>{capitalize(props.alert.alertType)}: </strong>
+            {props.alert.alertMessage}
+          </div>
         </div>
-      </div>
-    )
+      )}
+    </div>
   );
 };
